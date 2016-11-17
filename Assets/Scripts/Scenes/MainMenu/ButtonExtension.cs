79750b0aa47a32af6670d72e2496048beebdaf91
Finduilas <guillaume.mauger.src08@gmail.com>
2016-11-17 19:34:35 +0100

    Initial commit

---
+++ Assets/Scripts/Scenes/MainMenu/ButtonExtension.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonExtension : MonoBehaviour {
+
+	public Text label;
+	public Image logo;
+	public Button btnScript;
+	private Color localcolor;
+	private bool localInteractable;
+
+	// Use this for initialization
+	void Start () {
+		localInteractable = btnScript.interactable;
+		MouseExit (); //call for init color
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (localInteractable != btnScript.interactable) {
+			localInteractable = btnScript.interactable;
+			MouseExit ();
+			//on passe ici si le bouton devient interactable ou non. On peut donc en changer l'etat ca repercute la couleur sur le reste.
+		}
+		logo.color = localcolor;
+		label.color = localcolor;
+	}
+
+	//TODO : Gerer la couleur au click.
+
+	public void MouseEnter() {
+		Debug.Log ("on mouse enter");
+		if (localInteractable) {
+			localcolor = btnScript.colors.highlightedColor;
+		} else {
+			localcolor = btnScript.colors.disabledColor;
+		}
+	}
+	public void MouseExit() {
+		Debug.Log ("on mouse exit");
+		if (localInteractable) {
+			localcolor = btnScript.colors.normalColor;
+		} else {
+			localcolor = btnScript.colors.disabledColor;
+		}
+	}
+}
---
+++ Assets/Scripts/Scenes/MainMenu/MainMenuUIBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void BtnPNJAmbClicked(){
+		SceneManager.LoadScene ("PNJAmbEditor");
+	}
+
+	public void BtnPNJAdvClicked(){
+		Debug.Log ("btn pnj advanced clicked");
+	}
+
+	public void BtnObjClicked(){
+		Debug.Log ("btn Obj clicked");
+	}
+
+	public void BtnMonstersClicked(){
+		Debug.Log ("btn monster clicked ");
+	}
+
+	public void BtnMapClicked(){
+		Debug.Log ("btn map clicked ");
+	}
+
+	public void BtnCraftClicked(){
+		Debug.Log ("btn craft clicked");
+	}
+}
